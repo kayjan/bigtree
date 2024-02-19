aca2b01a59890ab68a182b3e2eae0731190b3678
Kay <kayjanw@gmail.com>
2024-02-20 01:05:38 +0800

    Changed: Pytest benchmark results on commits

--- tests/node/test_node_benchmark.py
+++ tests/node/test_node_benchmark.py
@@ -25,7 +25,7 @@ def run_construct_node(depth: int, width: int = 1, parent_node: Node = None) -> 
 
 
 def test_node_benchmark_width_1_depth_10(benchmark):
-    benchmark.pedantic(run_construct_node, (1000, 1), iterations=10, rounds=2)
+    benchmark.pedantic(run_construct_node, (10, 1), iterations=10, rounds=2)
 
 
 def test_node_benchmark_width_1_depth_100(benchmark):
