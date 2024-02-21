3ccf13caf63d50a8ed7612152f339e95f3b04d82
github-action-benchmark <github@users.noreply.github.com>
2024-02-21 10:13:20 +0000

    add Benchmark (pytest) benchmark result for 996f8c8c4408d5bc5d3c679ef9de521e4d55b82f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708510234242,
+  "lastUpdate": 1708510400860,
   "repoUrl": "https://github.com/kayjan/bigtree",
   "entries": {
     "Benchmark": [
@@ -76,6 +76,86 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00022790299044748871",
             "extra": "mean: 14.260742150000283 msec\nrounds: 2"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kayjanw@gmail.com",
+            "name": "Kay Jan W",
+            "username": "kayjan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "996f8c8c4408d5bc5d3c679ef9de521e4d55b82f",
+          "message": "Merge pull request #204 from kayjan/benchmark-gh-pages\n\nChanged: Auto push benchmark",
+          "timestamp": "2024-02-21T18:12:26+08:00",
+          "tree_id": "21a1c73cb14dc93be5e20c48f168f97550a4f3e2",
+          "url": "https://github.com/kayjan/bigtree/commit/996f8c8c4408d5bc5d3c679ef9de521e4d55b82f"
+        },
+        "date": 1708510399081,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/node/test_node_benchmark.py::test_node_benchmark_width_1_depth_10",
+            "value": 13822.569352926974,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001577095609453049",
+            "extra": "mean: 72.345450000455 usec\nrounds: 2"
+          },
+          {
+            "name": "tests/node/test_node_benchmark.py::test_node_benchmark_width_1_depth_100",
+            "value": 458.81327304583516,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00005117020648500351",
+            "extra": "mean: 2.179535900000218 msec\nrounds: 2"
+          },
+          {
+            "name": "tests/node/test_node_benchmark.py::test_node_benchmark_width_1_depth_1000",
+            "value": 6.635257643932447,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0012636873578007238",
+            "extra": "mean: 150.7100482999995 msec\nrounds: 2"
+          },
+          {
+            "name": "tests/node/test_node_benchmark.py::test_node_benchmark_width_2_depth_10",
+            "value": 54.96833237268153,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00005966489237913353",
+            "extra": "mean: 18.19229285000077 msec\nrounds: 2"
+          },
+          {
+            "name": "tests/node/test_node_benchmark.py::test_node_benchmark_width_1_depth_10_no_assertions",
+            "value": 22946.305644885448,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000005178567223104361",
+            "extra": "mean: 43.579999999820984 usec\nrounds: 2"
+          },
+          {
+            "name": "tests/node/test_node_benchmark.py::test_node_benchmark_width_1_depth_100_no_assertions",
+            "value": 1550.5086327289694,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000017207443524561313",
+            "extra": "mean: 644.9496499996599 usec\nrounds: 2"
+          },
+          {
+            "name": "tests/node/test_node_benchmark.py::test_node_benchmark_width_1_depth_1000_no_assertions",
+            "value": 129.96556088089432,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00021827544878169974",
+            "extra": "mean: 7.694346050000433 msec\nrounds: 2"
+          },
+          {
+            "name": "tests/node/test_node_benchmark.py::test_node_benchmark_width_2_depth_10_no_assertions",
+            "value": 86.5750333715324,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005221608105362948",
+            "extra": "mean: 11.550674150000617 msec\nrounds: 2"
           }
         ]
       }
