f9ecae3341741c117f19828c34462910bd09901f
Kay Jan W <kayjanw@gmail.com>
2024-07-09 18:03:58 +0800

    bump: v0.19.3

--- bigtree/__init__.py
+++ bigtree/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.19.2"
+__version__ = "0.19.3"
 
 from bigtree.binarytree.construct import list_to_binarytree
 from bigtree.dag.construct import dataframe_to_dag, dict_to_dag, list_to_dag
